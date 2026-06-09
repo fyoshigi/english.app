98bd6cba1b65f1bb3c02823676d721c8576dbe87
fyoshigi <2410990040n@hiro.kindai.ac.jp>
2026-06-09 15:33:45 +0900

    add words.csv

--- app.py
+++ app.py
@@ -1,19 +1,63 @@
 import streamlit as st
+import pandas as pd
+import random
 
-st.title("英単語帳アプリ")
+st.title("TOEIC単語クイズ")
 
-if "words" not in st.session_state:
-    st.session_state.words = []
+# CSV読み込み
+df = pd.read_csv("words.csv")
+words = df.to_dict("records")
 
-word = st.text_input("英単語")
-meaning = st.text_input("意味")
+# 初回設定
+if "score" not in st.session_state:
+    st.session_state.score = 0
 
-if st.button("登録"):
-    st.session_state.words.append(
-        {"word": word, "meaning": meaning}
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+if "quiz" not in st.session_state:
+    st.session_state.quiz = random.choice(words)
+
+# 問題表示
+st.subheader("問題")
+
+st.write(
+    f"英単語：{st.session_state.quiz['word']}"
+)
+
+answer = st.text_input("意味を入力")
+
+# 答え合わせ
+if st.button("答え合わせ"):
+
+    st.session_state.count += 1
+
+    if answer.strip() == st.session_state.quiz["meaning"]:
+
+        st.success("正解！")
+        st.session_state.score += 1
+
+    else:
+
+        st.error(
+            f"不正解！正解は {st.session_state.quiz['meaning']}"
+        )
+
+    st.session_state.quiz = random.choice(words)
+
+    st.rerun()
+
+# 成績
+if st.session_state.count > 0:
+
+    rate = (
+        st.session_state.score
+        / st.session_state.count
+        * 100
     )
 
-st.subheader("登録単語一覧")
+    st.write(f"正解数: {st.session_state.score}")
+    st.write(f"問題数: {st.session_state.count}")
+    st.write(f"正答率: {rate:.1f}%")
 
-for w in st.session_state.words:
-    st.write(f"{w['word']} : {w['meaning']}")
+    #python -m streamlit run app.py アプリ起動
